0bfc2001d788e8a7b9f0f9cd38a0fc686f8d47b6
Diego Alexander Páez Zapata <55529398+DiegoPaez2@users.noreply.github.com>
2023-06-30 09:30:52 -0500

    modificado main (#1)
    Agregado

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from models.Person import Person
 
 if __name__ == "__main__":
 
-    user=Person("Antoni Rodrigo","Toapanta Analuisa","0504110438")
+    user=Person("Diego Alexnader","Paez Zapata","1750343210")
     user.toString()
