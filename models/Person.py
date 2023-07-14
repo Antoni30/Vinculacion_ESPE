2cb2c8cbd9e78fb8ab007c49dfb5c63bff11439c
DiegoPaez2 <diegolexx99@gmail.com>
2023-07-14 08:59:10 -0500

    Creation funtion tostring of the person class

--- models/Person.py
+++ models/Person.py
@@ -16,6 +16,21 @@ class Person():
     def toString(self):
         print("names: {}\nlastnames: {}\nidCard: {}".format(self.names,self.lastnames,self.idCard))
 
+    def tostring(self):
+        """
+        Function tostring of the person class
+
+        Parameters
+        ---------------------------------------
+        none:none
+
+        Returns:
+        --------------------------------------
+        Returns a string representation of the person.
+        """
+        return f"names: {self.names}, lastname: {self.lastnames}, idCard: {self.idCard}"
+    
+    
     def greet(self):
         """
         Function that allows you to greet the person
@@ -28,6 +43,7 @@ class Person():
         ---------------------------------------
         not return anything
         """
+
         print('Hello ', self.names)
 
     
