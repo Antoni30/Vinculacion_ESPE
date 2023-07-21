d2bcb15dfc1545ea7c9c857a1678912e8cd0da60
Antoni30 <Antoni30@github.com>
2023-07-21 09:16:11 -0500

    first viwer

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-from models.Person import Person
-from models.Internal import Internal
-from models.External import External
+from views.UserViwer import UserViwer
 
 if __name__ == "__main__":
-
-    user=Person("Diego Alexnader","Paez Zapata","1750343210","dapaez3@espe.edu.ec")
-    user.toString()
-    user.greet()
-    user_internal = Internal("Jimena","Tutillo","1750463278","jstutillo@espe.edu.ec","L03532","student","matriz","administrativa")
-    user_internal.greet_internal()
-
-    user_external = External("Diego Alexnader","Paez Zapata","1750343210","dapaez3@espe.edu.ec","Sangolqui","10:20am","11:20am","1 hour")
-    user_external.gree_external()
+    UserViwer()
