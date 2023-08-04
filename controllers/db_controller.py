d6ed3844e0bc315d2699e2c519cd20a8c7774999
Antoni30 <Antoni30@github.com>
2023-08-04 10:57:44 -0500

    Actualizar

--- controllers/db_controller.py
+++ controllers/db_controller.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 import json
-from ..models.Internal import Internal
+
 
 uri = "mongodb+srv://registroespedb:Jpb3GO9zl7fQTlsu@cluster0.2lmju3k.mongodb.net/?retryWrites=true&w=majority"
 
@@ -14,17 +14,12 @@ try:
 except Exception as e:
     print(e)
 
-user_toni = Internal(campus='matriz', 
-         career='software',
-         ci='1725205106',
-         email='ttoapanta1@espe.edu.ec',
-         id_espe='L00001',
-         lastnames='Toapanta',
-         names='Toni',
-         type_espe='student'
-        )
+def usuario(userregister):
+    return userregister
 
-def add_user_db(user: Internal):
+user_toni = usuario(userregister=any)
+
+def add_user_db(user):
     db = client['sistema_registro_espe_db']
     collection = db['users']
     insert_result = collection.insert_one({
@@ -39,5 +34,3 @@ def add_user_db(user: Internal):
     })
     client.close()
     return insert_result.inserted_id
-id_resultante = add_user_db(user_toni)
-print("Documento insertado con ID: ", id_resultante)
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from views.Login import Login
+from models.Internal import Internal
+from controllers.db_controller import *
 
 if __name__ == "__main__":
     
-    login = Login()
-    login.mainloop()
+    # login = Login()
+    # login.mainloop()
+    user = Internal(campus='matriz', 
+         career='software',
+         ci='1725205106',
+         email='ttoapanta1@espe.edu.ec',
+         id_espe='L00001',
+         lastnames='Toapanta',
+         names='Toni',
+         type_espe='student'
+        )
+    usuarioB= usuario(user)
+    add_user_db(usuarioB)
+    
+    
+
